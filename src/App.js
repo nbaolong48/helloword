8402b2d12cd3b60e4098954c59026ee93483e41d
Long Nguyen Bao <long.nguyen@idealogic.com.vn>
2022-09-21 14:40:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Hello from docker
+        Hello word - az 204
       </header>
     </div>
   );
